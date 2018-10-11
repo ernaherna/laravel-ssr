f7efc78b08673c01f9b7e332affb275cb3d737a1
anugrah sandi <makassarnetwork06@gmail.com>
2018-10-11 21:15:56 +0800

    laravel ssr

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <ul>
+            <li>Laravel</li>
+            <li>Vuejs</li>
+            <li>Daengweb</li>
+            <li>Media Pemrograman Makassar</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
